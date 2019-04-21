4cb4e5130a587ec56c57def38ac7c7d9e8cf6b86
Piotr Dobiech <pitcer@protonmail.com>
2019-04-21 21:42:20 +0200

    Replace plugin.yml with plugin-yml gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	java
 	id("com.github.johnrengelman.shadow") version "5.0.0"
+	id("net.minecrell.plugin-yml.bukkit") version "0.3.0"
 }
 
 java {
@@ -17,4 +18,12 @@ dependencies {
 	implementation("com.flowpowered:flow-math:1.0.3")
 	compileOnly("com.destroystokyo.paper:paper-api:1.13.2-R0.1-SNAPSHOT")
 	compileOnly("org.jetbrains:annotations:17.0.0")
+}
+
+bukkit {
+	name = properties["plugin-name"] as String
+	main = "com.thevoxelbox.voxelsniper.VoxelSniperPlugin"
+	authors = listOf("przerwap", "MikeMatrix", "Gavjenks", "giltwist", "psanker", "Deamon5550", "DivineRage")
+	softDepend = listOf("VoxelModPackPlugin")
+	apiVersion = "1.13"
 }
