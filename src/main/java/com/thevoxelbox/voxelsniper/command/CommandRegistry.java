5dbfebe5608ad2d867878a1434e135a1dc0c3dd8
Jesse Boyd <jessepaleg@gmail.com>
2019-12-25 05:26:33 +0000

    craftserver, not server

--- src/main/java/com/thevoxelbox/voxelsniper/command/CommandRegistry.java
+++ src/main/java/com/thevoxelbox/voxelsniper/command/CommandRegistry.java
@@ -30,7 +30,7 @@ public class CommandRegistry {
 	@SuppressWarnings("JavaReflectionMemberAccess")
 	private CommandMap getCommandMap(Server server) {
 		try {
-			Method method = Server.class.getDeclaredMethod("getCommandMap");
+			Method method = server.getClass().getDeclaredMethod("getCommandMap"); // FAWE modify
 			return (CommandMap) method.invoke(server);
 		} catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException exception) {
 			throw new RuntimeException(exception);
