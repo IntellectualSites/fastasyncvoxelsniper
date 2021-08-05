b200c510ad8d5bb95b2b68cdef6c32cf38a5b2c5
Aurelien30000 <aurelien.domino@gmail.com>
2021-08-05 22:39:55 +0200

    Improve and fix ``#getLastBlock()`` method - Previously returned the wrong block when performing actions far from the ground (null targetBlock in Bukkit interaction event).

--- src/main/java/com/thevoxelbox/voxelsniper/sniper/toolkit/BlockTracer.java
+++ src/main/java/com/thevoxelbox/voxelsniper/sniper/toolkit/BlockTracer.java
@@ -2,22 +2,32 @@ package com.thevoxelbox.voxelsniper.sniper.toolkit;
 
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.util.Location;
+import com.sk89q.worldedit.util.TargetBlock;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Nullable;
 
 public class BlockTracer {
 
     private final BlockVector3 targetBlock;
+    private final BlockVector3 lastBlock;
 
-    public BlockTracer(Player player, int distance, boolean useLastBlock) {
+    public BlockTracer(Player player, int distance) {
         com.sk89q.worldedit.entity.Player fp = BukkitAdapter.adapt(player);
-        com.sk89q.worldedit.util.Location location = fp.getBlockTrace(distance, useLastBlock);
+        TargetBlock tracer = new TargetBlock(fp, distance, 0.2);
+        Location targetLocation = tracer.getAnyTargetBlock();
+        Location lastLocation = tracer.getPreviousBlock();
 
-        this.targetBlock = location == null ? null : location.toBlockPoint();
+        this.targetBlock = targetLocation == null ? null : targetLocation.toBlockPoint();
+        this.lastBlock = lastLocation == null ? null : lastLocation.toBlockPoint();
     }
 
     public @Nullable BlockVector3 getTargetBlock() {
         return this.targetBlock;
+    }
+
+    public @Nullable BlockVector3 getLastBlock() {
+        return lastBlock;
     }
 
 }
