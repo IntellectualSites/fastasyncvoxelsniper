bf8f8d203c5e11800523de0cf2d525956107611e
NotMyFault <mc.cache@web.de>
2021-08-18 10:45:33 +0200

    Add javadoc linking

--- build.gradle.kts
+++ build.gradle.kts
@@ -125,6 +125,9 @@ tasks {
         )
         opt.destinationDirectory = javadocDir
         opt.addBooleanOption("html5", true)
+        opt.links("https://papermc.io/javadocs/paper/1.17/")
+        opt.links("https://ci.athion.net/job/FastAsyncWorldEdit-1.17-Javadocs/javadoc/")
+        opt.links("https://javadoc.io/doc/org.jetbrains/annotations/22.0.0/")
     }
 
     jar {
