2f9eb34bb93ffa2603d39a835e0eae1064c9022c
NotMyFault <mc.cache@web.de>
2021-08-20 17:42:16 +0200

    Don't shadow twice

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,23 +75,6 @@ bukkit {
 	description = "World editing from ingame using 3D brushes"
 }
 
-tasks.named<ShadowJar>("shadowJar") {
-	archiveClassifier.set(null as String?)
-	dependencies {
-		relocate("org.incendo.serverlib", "com.thevoxelbox.voxelsniper.serverlib") {
-			include(dependency("org.incendo.serverlib:ServerLib:2.2.1"))
-		}
-		relocate("org.bstats", "com.thevoxelbox.voxelsniper.metrics") {
-			include(dependency("org.bstats:bstats-bukkit:2.2.1"))
-			include(dependency("org.bstats:bstats-base:2.2.1"))
-		}
-        relocate("io.papermc.lib", "com.thevoxelbox.voxelsniper.paperlib") {
-            include(dependency("io.papermc:paperlib:1.0.6"))
-        }
-	}
-	minimize()
-}
-
 val javadocDir = rootDir.resolve("docs").resolve("javadoc")
 tasks {
     val assembleTargetDir = create<Copy>("assembleTargetDirectory") {
@@ -142,6 +125,10 @@ tasks {
         this.archiveClassifier.set(null as String?)
         this.archiveFileName.set("${project.name}-${project.version}.${this.archiveExtension.getOrElse("jar")}")
         this.destinationDirectory.set(rootProject.tasks.shadowJar.get().destinationDirectory.get())
+        relocate("org.incendo.serverlib", "com.thevoxelbox.voxelsniper.serverlib")
+        relocate("org.bstats", "com.thevoxelbox.voxelsniper.metrics")
+        relocate("io.papermc.lib", "com.thevoxelbox.voxelsniper.paperlib")
+        minimize()
     }
 
     named("build") {
