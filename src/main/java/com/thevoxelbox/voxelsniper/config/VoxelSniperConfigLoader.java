cb71f1d9bdb4a0effb10402d14ad782a3400d005
Aurélien <43724816+Aurelien30000@users.noreply.github.com>
2021-09-03 21:40:50 +0100

    Update VoxelSniperConfigLoader.java
    ``DEFAULT_MESSAGE_ON_LOGIN_ENABLED`` must match config file, i.e. ``true``

--- src/main/java/com/thevoxelbox/voxelsniper/config/VoxelSniperConfigLoader.java
+++ src/main/java/com/thevoxelbox/voxelsniper/config/VoxelSniperConfigLoader.java
@@ -33,7 +33,7 @@ public class VoxelSniperConfigLoader {
     protected static final String BRUSH_PROPERTIES = "brush-properties";
 
     private static final int CONFIG_VERSION_VALUE = 1;
-    private static final boolean DEFAULT_MESSAGE_ON_LOGIN_ENABLED = false;
+    private static final boolean DEFAULT_MESSAGE_ON_LOGIN_ENABLED = true;
     private static final BlockType DEFAULT_BLOCK_MATERIAL_VALUE = BlockTypes.AIR;
     private static final BlockType DEFAULT_REPLACE_BLOCK_MATERIAL_VALUE = BlockTypes.AIR;
     private static final int DEFAULT_BRUSH_SIZE_VALUE = 3;
