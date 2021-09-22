a477d1ec540ada6fce678a87ca54b43d23b6fbc9
NotMyFault <mc.cache@web.de>
2021-09-22 13:38:26 +0200

    Add `/vs debugpaste`

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
 	implementation(libs.bstatsBase)
 	implementation(libs.bstatsBukkit)
     implementation(libs.paperlib)
+    implementation(libs.paster)
 }
 
 tasks.compileJava.configure {
@@ -106,6 +107,7 @@ tasks {
         relocate("org.incendo.serverlib", "com.thevoxelbox.voxelsniper.serverlib")
         relocate("org.bstats", "com.thevoxelbox.voxelsniper.metrics")
         relocate("io.papermc.lib", "com.thevoxelbox.voxelsniper.paperlib")
+        relocate("com.intellectualsites.paster", "com.thevoxelbox.voxelsniper.paster")
         minimize()
     }
 
